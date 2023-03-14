7fb6e096f871a9888106b7cfc69c6b6283e65608
Voltron7 <aquastar.minsk@gmail.com>
2023-03-14 11:53:53 +0300

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,59 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class MemoryCandidateRepository implements CandidateRepository {
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+    private int nextId = 1;
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "John", "description1", LocalDateTime.now()));
+        save(new Candidate(0, "Paul", "description2", LocalDateTime.now()));
+        save(new Candidate(0, "Bill", "description3", LocalDateTime.now()));
+        save(new Candidate(0, "Rose", "description4", LocalDateTime.now()));
+        save(new Candidate(0, "Mila", "description5", LocalDateTime.now()));
+        save(new Candidate(0, "Maria", "description6", LocalDateTime.now()));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return candidates.remove(id) != null;
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(), (id, oldCandidate) -> new Candidate(
+                oldCandidate.getId(), candidate.getName(), candidate.getDescription(),
+                candidate.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
