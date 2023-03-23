d5026ff8cf9f761716814c3c831174caa3135d6a
Voltron7 <aquastar.minsk@gmail.com>
2023-03-23 14:02:23 +0300

    2. CandidateRepository. Хранение кандидатов. [#504861]

--- src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.dto.FileDto;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.repository.CandidateRepository;
-
 import java.util.Collection;
 import java.util.Optional;
 
