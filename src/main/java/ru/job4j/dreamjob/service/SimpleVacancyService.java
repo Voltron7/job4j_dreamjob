51da38c0a9384d21de6b9dbffc9871d7d24a725a
Voltron7 <aquastar.minsk@gmail.com>
2023-03-23 12:21:23 +0300

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]

--- src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.dto.FileDto;
 import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.repository.VacancyRepository;
-
 import java.util.Collection;
 import java.util.Optional;
 
